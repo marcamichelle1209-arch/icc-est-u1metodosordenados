012e1ace8f03342b6848f9d6937bf9d214a4780b
marcamichelle1209-arch <marcamichelle1209@gmail.com>
2026-04-20 19:53:04 -0500

    Metodo bubble Sort

--- README.md
+++ README.md
@@ -1,18 +1,13 @@
-## Getting Started
+# Universidad Politecnica Salesiana
 
-Welcome to the VS Code Java world. Here is a guideline to help you get started to write Java code in Visual Studio Code.
+## Estructura de Datos
+## Estudiante: Michelle Marca
 
-## Folder Structure
 
-The workspace contains two folders by default, where:
+## Practica 1.1 Metodo Burbuja
+### Fecha: 20/04/2026
 
-- `src`: the folder to maintain sources
-- `lib`: the folder to maintain dependencies
+### Descripcion:
 
-Meanwhile, the compiled output files will be generated in the `bin` folder by default.
+Cree la clase ` Metodo Burbuja ` que implanta metodo de ordenamiento ascedente,descente y un metodo para imprimir con constructor que instancia una variable global.
 
-> If you want to customize the folder structure, open `.vscode/settings.json` and update the related settings there.
-
-## Dependency Management
-
-The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
---
+++ src/MetodoBurbuja.java
@@ -0,0 +1,57 @@
+public class MetodoBurbuja { 
+
+
+
+
+    int[] arreglo;  //global lista de enteros
+
+    //Constructor vacio
+
+    public MetodoBurbuja(int[]arreglo){
+        //System.out.//println("Metodo Burbuja");
+        // 1 forma
+        // Instanciar con valor creado en constructor
+        //this.arreglo = new int []{10,5,8,0,7};
+        // 2 forma
+        // Instanciar con valor desde parametro
+        this.arreglo = arreglo;
+    }
+    public void ordenarAscendente(){
+        for (int i =0; i<arreglo.length;i++){
+            for (int j =i+1; i<arreglo.length;j++){
+                if(arreglo[i] > arreglo [j]){
+                    int aux = arreglo[i];
+                    arreglo [i] =arreglo [j];
+                    arreglo [j] = aux;
+                }
+            }
+        }
+    }
+
+    public void imprimirArreglo(){
+        for(int num: arreglo){
+            System.out.print(num + ",");
+        }
+        System.out.println();
+    }
+
+    public void ordenarDescendente(){
+        for (int i =0; i<arreglo.length;i++){
+            for (int j =i+1; i<arreglo.length;j++){
+                if(arreglo[i] <  arreglo [j]){
+                    int aux = arreglo[i];
+                    arreglo [i] =arreglo [j];
+                    arreglo [j] = aux;
+                }
+            }
+        }
+    }
+    public void imprimirArreglo(){
+        for(int num: arreglo){
+            System.out.print(num + ",");
+        }
+        System.out.println();
+    }
+   
+    
+}
